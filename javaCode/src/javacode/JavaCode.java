6b2a74c3d8efdd66d5d064a247ff89adcab6b123
Mr-dang <yuki910520@gmail.com>
2019-05-24 10:16:39 +0800

    初始文件

--- javaCode/src/javacode/JavaCode.java
+++ javaCode/src/javacode/JavaCode.java
@@ -5,6 +5,8 @@
  */
 package javacode;
 
+import java.io.*;
+
 /**
  *
  * @author dang
@@ -14,8 +16,17 @@ public class JavaCode {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        char c;
+        // 使用 System.in 创建 BufferedReader 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter characters, 'q' to quit.");
+        // 读取字符
+        do {
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
     }
-    
+
 }
